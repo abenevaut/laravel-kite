a77bb27cc7737f7d968960665853ee298c64a91f
abenevaut <me@abenevaut.dev>
2023-05-14 14:39:42 +0000

    Update master to output generated at 64153df

--- src/App/Providers/KiteServiceProvider.php
+++ src/App/Providers/KiteServiceProvider.php
@@ -24,6 +24,5 @@ class KiteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../../../migrations');
-        $this->loadRoutesFrom(__DIR__ . '/../../../routes/web.php');
     }
 }
