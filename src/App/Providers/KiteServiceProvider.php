1bb8520a08f5d4b17318b74daadf8249be3172ba
abenevaut <me@abenevaut.dev>
2023-11-30 20:45:52 +0000

    Update master to output generated at b11cb5a

--- src/App/Providers/KiteServiceProvider.php
+++ src/App/Providers/KiteServiceProvider.php
@@ -9,6 +9,6 @@ class KiteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../../../migrations');
-        $this->mergeConfigFrom(__DIR__ . '/../../../config/auth.php');
+        $this->mergeConfigFrom(__DIR__ . '/../../../config/auth.php', 'auth');
     }
 }
